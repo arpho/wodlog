1dff13c0e42ef3aba00c6596ac5e77b521dbca31
arpho <damicogiuseppe77@gmail.com>
2025-01-18 17:16:43 +0100

    after wod creation we are redirect to the wod-list

--- src/app/pages/wod/create/create-wod/create-wod.page.ts
+++ src/app/pages/wod/create/create-wod/create-wod.page.ts
@@ -78,6 +78,9 @@ export class CreateWodPage implements OnInit {
           duration: 2000,
         });
         toast.present();
+        toast.onDidDismiss().then(() => {
+         this.router.navigate(['/wods-list'])
+        })
       });
   }
   wod = new WodModel();
