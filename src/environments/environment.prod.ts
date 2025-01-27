6bb7101b7fa2c2d5af213a8ec6b11b4135d0a963
arpho <damicogiuseppe77@gmail.com>
2025-01-27 11:04:58 +0100

    added missing configuration for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDh_aGD_ksup-PasR7ewLjHjZ7qHjoSzgQ",
+    authDomain: "m1crossfit-5b2b9.firebaseapp.com",
+    databaseURL: "https://m1crossfit-5b2b9.firebaseio.com",
+    projectId: "m1crossfit-5b2b9",
+    storageBucket: "m1crossfit-5b2b9.firebasestorage.app",
+    messagingSenderId: "959204307943",
+    appId: "1:959204307943:web:24ed9f53f6b914d0608342"
+  },
+  backendUrl: 'http://localhost:3000'
 };
